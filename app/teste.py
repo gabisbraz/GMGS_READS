315f6760a203c0f2d6e81e106c28fb53f99c48d9
Maria Julia de Pádua <65764506+majupadua@users.noreply.github.com>
2024-09-24 15:25:55 -0300

    Update teste.py

--- app/teste.py
+++ app/teste.py
@@ -1,9 +1,9 @@
 def is_prime(num):
     if num <= 1:
         return False
-    for i in range(2, int(num ** 0.2) + 1):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            return False
+            return Fals
     return True
 
 def test_prime_numbers():
